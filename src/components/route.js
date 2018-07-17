7d7f174692f9f69eecd43d9aa3eaa05e539ce569
wzy4072 <wzy4072@163.com>
2018-07-17 08:46:49 +0800

    add routes

---
+++ src/components/route.js
@@ -0,0 +1,37 @@
+// import Login from './login.vue'
+// import Register from './register.vue'
+// import Welcome from './welcome.vue'
+// import Stulist from './stuList.vue'
+// import StuDetial from './stuDetial.vue'
+// import Qrcode from './qrcode.vue'
+// import Home from './home/welcome.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'defaultLogin',
+//     component: Login
+//   }, {
+//     path: '/login',
+//     name: 'login',
+//     component: Login
+//   },
+//   {
+//     path: '/reg',
+//     name: 'register',
+//     component: Register
+//   }, {
+//     path: '/home',
+//     // name: 'Home',
+//     component: Home,
+//     children: [
+//       { path: '/Welcome', name: 'Welcome', component: Welcome },
+//       { path: '/stumanage/list', name: 'Stulist', component: Stulist },
+//       { path: '/stumanage/detial/:id', name: 'StuDetial', component: StuDetial },
+//       { path: '/qrcode', name: 'Qrcode', component: Qrcode }
+//     ]
+//   }
+// ]
+// export default {
+//   routes
+// }
